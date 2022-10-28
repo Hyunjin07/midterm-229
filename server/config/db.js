960c571e244c4085ff2fcb103e4c5f5bce5fcac7
Hyunjin07 <111410288+Hyunjin07@users.noreply.github.com>
2022-10-28 09:32:35 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  URI: "mongodb+srv://newuser:HYUNjin0725@midtermcluster.6abeghs.mongodb.net/car_store??retryWrites=true&w=majority",
+};
---
+++ server/routes/cars.js
@@ -0,0 +1,142 @@
+// modules required for routing
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+
+// define the car model
+let car = require("../models/cars");
+
+/* GET cars List page. READ */
+router.get("/", (req, res, next) => {
+  // find all cars in the cars collection
+  car.find((err, cars) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("cars/index", {
+        title: "Cars",
+        cars: cars,
+      });
+    }
+  });
+});
+
+//  GET the Car Details page in order to add a new Car
+router.get("/add", (req, res, next) => {
+ 
+  car.find((err, cars) => {
+      if (err) {
+          return console.error(err);
+          res.end(err);
+      } else {
+          res.render("cars/add", {
+              title: "Cars",
+              cars: cars,
+          });
+      }
+  });
+
+});
+// POST process the Car  Details page and create a new Car  - CREATE
+router.post("/add", (req, res, next) => {
+  let newCar = car({
+    Carname: req.body.Carname,
+    Category : req.body.Category,
+    Carmodel : req.body.Carmodel,
+    Price : req.body.Price
+    
+  });
+  car.create(newCar,(err,car) =>{
+    if(err){
+      console.log(err);
+      res.end(err);
+    }
+    else
+    {
+      res.redirect('/cars');
+    }
+  });
+});
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+  // GET the Car Details page in order to edit an existing Car
+router.get("/edit/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  car.findById(id,(err,carTodetail) => {
+    if(err)
+    {
+      console.log(err);
+      res.end(err);
+
+    }
+    else{
+      res.render('cars/edit',{title:'Edit Car', cars: carTodetail})
+    }
+  });
+});
+  
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+
+// POST - process the information passed from the details form and update the document
+router.post("/edit/:id", (req, res, next) => {
+  let id = req.params.id
+
+  let updateCar = car ({
+    _id: id,
+    Carname: req.body.Carname,
+    Category : req.body.Category,
+    Carmodel : req.body.Carmodel,
+    Price : req.body.Price
+    
+
+  });
+
+  car.updateOne({_id: id}, updateCar,(err) => {
+    if(err)
+    {
+      console.log(err);
+      res.end(err);
+    }else{
+       res.redirect('/cars');
+    }
+
+  });
+
+});
+  
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+
+// GET - process the delete
+router.get("/delete", (req, res, next) => {
+
+  let id = req.params.id;
+  car.remove({Price: {$gt:15000 }}, (err) =>{
+    if(err){
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect('/cars');
+    }
+  });
+
+});
+
+
+
+ 
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+
+module.exports = router;
